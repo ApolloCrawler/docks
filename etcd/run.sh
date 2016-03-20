060982eccd3c806f7987145c74ec6dfa2c187283
Tomas Korcak <korczis@gmail.com>
2016-03-20 22:52:22 +0100

    Remove old stuff

--- etcd/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# see https://github.com/coreos/etcd/blob/master/Documentation/docker_guide.md
-
-docker run -d -p 2379:2379 -p 2380:2380 -p 4001:4001 -p 7001:7001 --name etcd korczis/etcd
--- redis/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -p 6379:6379 -v /data --name redis korczis/redis
